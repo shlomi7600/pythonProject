27de007ac40cde258a14e9e0d9317db793acfb77
שלומי <shlomi7600@gmail.com>
2021-10-03 10:44:30 +0300

    new

---
+++ qa/3.1.py
@@ -0,0 +1,5 @@
+num=int(input("random num: "))
+
+if num%2==0:
+    print("zugi")
+else:print("lo zugi")
---
+++ qa/tnaim_1.py
@@ -0,0 +1,5 @@
+num=int(input("random num: "))
+num2=int(input("random number2: "))
+if (num+num2)%2==0:
+    print("zugi")
+else:print("lo zugi")
